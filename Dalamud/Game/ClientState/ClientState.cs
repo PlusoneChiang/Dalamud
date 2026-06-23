ea1819dd4b66902ade8bb0079bd787d02ca5fc28
Plusone Chiang <plusone@appx.com.tw>
2026-06-23 12:38:44 +0800

    change fallback lang

--- Dalamud/Game/ClientState/ClientState.cs
+++ Dalamud/Game/ClientState/ClientState.cs
@@ -379,7 +379,7 @@ internal class ClientStatePluginScoped : IInternalDisposableService, IClientStat
     /// <inheritdoc/>
     public ClientLanguage ClientLanguage =>
         this.clientStateService.ClientLanguage == ClientLanguage.TraditionalChinese
-            ? ClientLanguage.Japanese
+            ? ClientLanguage.SimplifiedChinese
             : this.clientStateService.ClientLanguage;
 
     /// <inheritdoc/>
